00429ed9bf8999012bd4729f60a9ead50630c7f2
Arsenii <cubelius@icloud.com>
2025-08-19 13:35:08 +0200

    feat: don't copy the assets directory

--- scripts/create_app_bundle.sh
+++ scripts/create_app_bundle.sh
@@ -76,10 +76,7 @@ cat > "$APP_PATH/Contents/Info.plist" << EOF
 </plist>
 EOF
 
-# Copy assets directory if it exists.
-if [[ -d "assets" ]]; then
-    cp -R "assets" "$APP_PATH/Contents/Resources/"
-fi
+
 
 # Copy app icon if it exists.
 if [[ -f "assets/icons/app_icon.icns" ]]; then
